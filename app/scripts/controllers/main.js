992e230375035eb8b327ba62eb6099d0147d2da2
Alex Chen <Alex.yf.Chen@outlook.com>
2016-02-29 15:15:54 -0500

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uwcookingclubApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uwcookingclubApp
+ */
+angular.module('uwcookingclubApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
